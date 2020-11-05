ba713c56b7322f036fb24a3df53b15ea326e41ee
Aram Ohanyan <aram.ohanyan@optym.com>
2020-11-05 13:52:38 +0400

    fix spring-batch mongo

--- batch-service/src/main/java/com/aram/batchservice/BatchServiceApplication.java
+++ batch-service/src/main/java/com/aram/batchservice/BatchServiceApplication.java
@@ -2,8 +2,9 @@ package com.aram.batchservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class BatchServiceApplication {
 
 	public static void main(String[] args) {
